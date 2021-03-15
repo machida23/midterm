0f60d2553f8ff047dbad3a2a27539c0219b27f75
machida23 <machida23@up.edu>
2021-03-15 15:44:47 -0700

    Initial

--- app/src/main/java/com/example/midterm/MainActivity.java
+++ app/src/main/java/com/example/midterm/MainActivity.java
@@ -10,8 +10,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button leftButton, rightButton;
     private TextView text;
-    private int random = 0;
-    private int clicks = 0;
+    private int random;
+    public int clicks = 0;
 
 
     @Override
@@ -22,16 +22,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         text = findViewById(R.id.textView);
         leftButton = findViewById(R.id.leftButton);
         rightButton = findViewById(R.id.rightButton);
+        random = (int) (2 * (Math.random()));
+
         leftButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                random = (int) (2 * (Math.random()));
+
                 if (random == 0) {
                     leftButton.setVisibility(v.VISIBLE);
-                    rightButton.setVisibility(v.GONE);
+
                 } else {
-                    leftButton.setVisibility(v.GONE);
+                    leftButton.setVisibility(v.INVISIBLE);
+
+                }
+                text.setText("Number of clicks: " + clicks);
+                clicks++;
+            }
+
+        });
+        rightButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if (random == 0) {
+                    rightButton.setVisibility(v.INVISIBLE);
+                } else {
+
                     rightButton.setVisibility(v.VISIBLE);
                 }
                 text.setText("Number of clicks: " + clicks);
@@ -39,7 +57,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
 
         });
-
     }
 
     @Override
